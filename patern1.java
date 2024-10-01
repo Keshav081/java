3456aba7d40511a9fc4d9f599f77c8d0c568970f
Keshav081 <148437456+Keshav081@users.noreply.github.com>
2024-09-30 22:08:47 -0700

    Update patern1.java

--- patern1.java
+++ patern1.java
@@ -1,7 +1,7 @@
 public class patern1 {
    
         public static void main(String[] args) {
-            for(int i=0;i<5;i++){
+            for(int i=0;i<8;i++){
                 for(int j=0;j<5;j++){
                     System.out.print("*");
                 }
